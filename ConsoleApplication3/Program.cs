77a1a50406d722a01c9d00b7051c50212cd22aaf
Diablx <kajetanboruta@gmail.com>
2020-02-08 17:29:41 +0100

    second commit

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ConsoleApplication3
 {
@@ -10,6 +11,14 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+
+           
+            crud.Create("śląsk", conn);
+            crud.Read(conn);
         }
     }
 }
---
+++ ConsoleApplication3/crud.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public static class crud
+    {
+        public static void Create(string regionDescription, SqlConnection conn)
+        {
+            string sql = "INSERT INTO northwind.dbo.Region(RegionId,RegionDescription) VALUES (5,'@regionName');";
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.Add("regionName", regionDescription);
+
+            int a = command.ExecuteNonQuery();
+            if (a > 0)
+            {
+                Console.WriteLine("Wpisano");
+            }
+        }
+
+        public static void Read(SqlConnection connect)
+        {
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "SELECT * FROM Northwind.dbo.Customers",
+                Connection = connect,
+            };
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+            }
+            reader.Close();
+        }
+
+        public static void Update(SqlConnection connect)
+        {
+            connect.Open();
+            string sql = "UPDATE Region SET RegionDescription = @regionName WHERE RegionId = @id";
+
+            var command = new SqlCommand(sql);
+            command.Parameters.AddWithValue("id", "");
+            command.Parameters.AddWithValue("regionName", "");
+
+            command.ExecuteNonQuery();
+
+            connect.Close();
+        }
+    }
+}
